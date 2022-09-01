c8018b9d365b73a6fa77fab1de6f8902e3a7fed5
s-darth <s2partheni@gmail.com>
2022-09-01 11:47:34 +0200

    Assignment

--- week_3/js/script.js
+++ week_3/js/script.js
@@ -8,41 +8,69 @@
 // choose[1]:'paper'
 // choose[2]:'scissors'
 
-const choices = ['rock', 'paper', 'scissor'];
+
+let scoreComputer = 0;
+let scorePlayer = 0;
 
 function computerPlay() {
+    const choices = ['rock', 'paper', 'scissor'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-// console.log(computerPlay());
 
 // c.
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection){
         return "It\s a Draw!";
-    } 
-    else if (playerSelection == "paper"){
-        return (computerSelection == "rock")? "You Win!Paper beats rock!" : "You Lose!"
-    }
-    else if (playerSelection == "scissor"){
-        return (computerSelection == "paper")? "You Win!Scissor beats paper!" : "You Lose!"
-    }
-    else if (playerSelection == "rock"){
-        return (computerSelection == "scissor")? "You Win!Rock beats scissor!" : "You Lose!"
+    } else if (playerSelection == "paper"){
+         return (computerSelection == "rock")? (scorePlayer += 1) : (scoreComputer += 1);
+    } else if (playerSelection == "scissor"){
+        return (computerSelection == "paper")? (scorePlayer += 1) : (scoreComputer += 1);
+    } else if (playerSelection == "rock"){
+        return (computerSelection == "scissor")? (scorePlayer += 1) : (scoreComputer += 1);
     }
 }
 
-const playerSelection = prompt("Please enter your choice here!!").toLowerCase();
-const computerSelection = computerPlay();
-console.log(`Computer chose: ${computerSelection}`);
-console.log(`Player chose: ${playerSelection}`);
-console.log(playRound(playerSelection,computerSelection));
+// Loop 5 times
+const game = () => {
+    for (i = 0; i < 2; i++) {
+        const playerSelection = prompt("Please enter your choice here!!").toLowerCase();
+        const computerSelection = computerPlay();
 
-// let game = () => {
-//     let count = 0;
-//     // for (i=0; i < 3; i++) {
-//     // } 
+        console.log(`YOU chose ${playerSelection} while the COMPUTER chose ${computerSelection}.`);
+        console.log(`The score of the COMPUTER is ${scoreComputer}, while YOURS is ${scorePlayer}.`);
+    }        
+    playRound();
+    
+    //  result comparison
+    if (scorePlayer > scoreComputer) {
+        alert("You win the game!Great job!");
+    } else if (scorePlayer < scoreComputer) {
+        alert("Computer wins the game!");
+    } else {
+        alert("Its a Tie!Nobody wins!");
+    }
+}
+game();
+
+// const playerSelection = prompt("Please enter your choice here!!").toLowerCase();
+// const computerSelection = computerPlay();
+
+// function game() {
+//     for (i = 0; i < 2; i++) {
+//         const playerSelection = prompt("Please enter your choice here!!").toLowerCase();
+//         const computerSelection = computerPlay();
+        
+//         console.log(`The COMPUTER chose ${computerSelection} and YOU chose ${playerSelection}.`);
+//         console.log(`The score of the COMPUTER is ${scoreComputer}, while YOURS is ${scorePlayer}.`);
+
+//         playRound();
+//     }
+//     if (scorePlayer > scoreComputer) {
+//         alert("You win the game!");
+//     } else if (scoreComputer > scorePlayer) {
+//         alert("Computer wins the game!");
+//     } else {
+//         alert("Its a Tie!");
+//     }
 // }
-// console.log(game());
-
-
-
+// game();
