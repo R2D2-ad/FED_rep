a652cce2c0cf9d65c377057e01441588962be0da
s-darth <s2partheni@gmail.com>
2022-09-01 13:11:21 +0200

    Assignment

--- week_3/js/script.js
+++ week_3/js/script.js
@@ -1,12 +1,4 @@
 // Rock - Paper - Scissors Game
-// Create a function called computerPlay 
-// that will randomly return either ‘rock’, ‘paper’ or ‘scissors’
-// b.
-// console.log(choose.length);
-// console.log(choose[0]);
-// choose[0]:'rock'
-// choose[1]:'paper'
-// choose[2]:'scissors'
 let scoreComputer = 0;
 let scorePlayer = 0;
 
@@ -17,36 +9,39 @@ function computerPlay() {
 
 // c.
 const playRound = (playerSelection, computerSelection) => {
-    if (playerSelection === computerSelection){
+    if (playerSelection == computerSelection){
         return "It\s a Draw!";
-    } else if (playerSelection == "paper"){
-         return (computerSelection == "rock")? (scorePlayer += 1) : (scoreComputer += 1);
-    } else if (playerSelection == "scissor"){
-        return (computerSelection == "paper")? (scorePlayer += 1) : (scoreComputer += 1);
-    } else if (playerSelection == "rock"){
-        return (computerSelection == "scissor")? (scorePlayer += 1) : (scoreComputer += 1);
+    } else if (playerSelection === "paper"){
+         return (computerSelection === "rock") ? (scorePlayer += 1) : (scoreComputer += 1);
+    } else if (playerSelection === "scissor"){
+        return (computerSelection === "paper") ? (scorePlayer += 1) : (scoreComputer += 1);
+    } else if (playerSelection === "rock"){
+        return (computerSelection === "scissor") ? (scorePlayer += 1) : (scoreComputer += 1);
+    } else {
+        return `It\s a tie!`;
     }
 }
 
 // Loop 5 times
 const game = () => {
     for (i = 0; i < 2; i++) {
-        const playerSelection = prompt("Please enter your choice here!!").toLowerCase();
+        const playerSelection = prompt("Let's play 'Rock - Paper - Scissors'!!").toLowerCase();
         const computerSelection = computerPlay();
     
         console.log(`YOU chose ${playerSelection} while the COMPUTER chose ${computerSelection}.`);
-        console.log(`The score of the COMPUTER is ${scoreComputer}, while YOURS is ${scorePlayer}.`);
                 
         playRound(playerSelection, computerSelection);
-    }
 
-    //  result comparison
-    if (scorePlayer > scoreComputer) {
-        alert("You win the game!Great job!");
-    } else if (scorePlayer < scoreComputer) {
-        alert("Computer wins the game!");
-    } else {
-        alert("Its a Tie!Nobody wins!");
+        //  result comparison
+        if (scorePlayer > scoreComputer) {
+            alert("You win the game!Great job!");
+            console.log(`YOURS is ${scorePlayer}, while the score of the COMPUTER is ${scoreComputer}.`);
+        } else if (scoreComputer > scorePlayer) {
+            alert("Computer wins the game!");
+            console.log(`The score of the COMPUTER is ${scoreComputer}, while YOURS is ${scorePlayer}.`);
+        } else if (scoreComputer === scorePlayer) {
+            alert("Its a Tie!Nobody wins!");
+        }
     }
 }
 game();
